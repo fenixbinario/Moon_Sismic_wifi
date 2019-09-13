5a5b548e295c7c86c51f9320e9c2302e319bf6b0
Fenix <fbamperius@gmail.com>
2019-09-13 11:31:44 +0200

    Agregar archivos de proyecto.

---
+++ Moon_Sismic_wifi/Moon_Sismic_wifi.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Moon_Sismic_wifi.ino
+ Created:	13/09/2019 11:31:40
+ Author:	Fenix
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
