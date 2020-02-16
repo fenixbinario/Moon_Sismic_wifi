dc9b327870184c33f08193af14f1eb397f792f43
Fenix <fbamperius@gmail.com>
2020-02-16 12:06:32 +0100

    Actualizacion!!

--- Moon_Sismic_wifi/Moon_Sismic_wifi.ino
+++ Moon_Sismic_wifi/Moon_Sismic_wifi.ino
@@ -1,15 +1,81 @@
 /*
  Name:		Moon_Sismic_wifi.ino
  Created:	13/09/2019 11:31:40
- Author:	Fenix
+ Author:	@fenixbinario
+ Web: www.fenixbinario.com
 */
 
-// the setup function runs once when you press reset or power the board
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <ESP8266HTTPClient.h>
+
+#include <WiFiClientSecureBearSSL.h>
+// Fingerprint for demo URL, expires on June 2, 2019, needs to be updated well before this date
+//const uint8_t fingerprint[20] = {0x5A, 0xCF, 0xFE, 0xF0, 0xF1, 0xA6, 0xF4, 0x5F, 0xD2, 0x11, 0x11, 0xC6, 0x1D, 0x2F, 0x0E, 0xBC, 0x39, 0x8D, 0x50, 0xE0};
+
+ESP8266WiFiMulti WiFiMulti;
+
 void setup() {
 
+	Serial.begin(115200);
+	// Serial.setDebugOutput(true);
+
+	Serial.println();
+	Serial.println();
+	Serial.println();
+
+	for (uint8_t t = 4; t > 0; t--) {
+		Serial.printf("[SETUP] WAIT %d...\n", t);
+		Serial.flush();
+		delay(1000);
+	}
+
+	WiFi.mode(WIFI_STA);
+	WiFiMulti.addAP("CYBORG", "delkano22");
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
-  
+	// wait for WiFi connection
+	if ((WiFiMulti.run() == WL_CONNECTED)) {
+
+		std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+
+		//client->setFingerprint(fingerprint);
+		client->setInsecure();
+		HTTPClient https;
+
+		Serial.print("[HTTPS] begin...\n");
+		if (https.begin(*client, "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-09-12&endtime=2019-09-13")) {  // HTTPS
+
+			Serial.print("[HTTPS] GET...\n");
+			// start connection and send HTTP header
+			int httpCode = https.GET();
+
+			// httpCode will be negative on error
+			if (httpCode > 0) {
+				// HTTP header has been send and Server response header has been handled
+				Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+
+				// file found at server
+				if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+					String payload = https.getString();
+					Serial.println(payload);
+				}
+			}
+			else {
+				Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+			}
+
+			https.end();
+		}
+		else {
+			Serial.printf("[HTTPS] Unable to connect\n");
+		}
+	}
+
+	Serial.println("Wait 10s before next round...");
+	delay(300000);
 }
